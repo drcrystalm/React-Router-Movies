5186402a1a7971fa19380ccbdf8e40827b46712d
Crystal Martin <drcrystalm@gmail.com>
2019-10-15 22:25:04 -0500

    it is basic but it works

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 import { Route } from "react-router-dom"
 
 import SavedList from "./Movies/SavedList"
+import MovieList from "./Movies/MovieList"
+import Movie from "./Movies/Movie"
 
 const App = () => {
     const [savedList, setSavedList] = useState([])
@@ -14,7 +16,11 @@ const App = () => {
         <div>
             <SavedList list={savedList} />
             <Route exact path='/' component={MovieList} />
-            <Route path='/movies/:movieID' component={Movie} />
+            <Route
+                path='/movies/:movieID'
+                component={Movie}
+                // render={props => <Movie {...props} />}
+            />
         </div>
     )
 }
